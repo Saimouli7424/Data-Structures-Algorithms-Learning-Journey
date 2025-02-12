1903a0942addcc02b41604fcf4a7808ffff0cb3b
Sai Mouli Bandi <saimoulispeed@gmail.com>
2025-02-12 21:57:18 +0530

    insertionsort

---
+++ Day4/insertionsort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void insertionSort(vector<int>& arr) {
+    int n = arr.size();
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    vector<int> arr = {5, 3, 8, 1, 2};
+    
+    insertionSort(arr);
+
+    cout << "Sorted Array: ";
+    for (int num : arr) cout << num << " ";
+    
+    return 0;
+}
