4aead0785ff8cd2f99f34e5469dd21029b3ec33f
Sai Mouli Bandi <saimoulispeed@gmail.com>
2025-02-13 21:57:23 +0530

    directed graph

---
+++ Day6/graphs.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Graph {
+    int V;
+    vector<vector<int>> adj;
+
+public:
+    Graph(int V) {
+        this->V = V;
+        adj.resize(V);
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].push_back(w);
+        adj[w].push_back(v);
+    }
+
+    void display() {
+        for (int i = 0; i < V; i++) {
+            cout << i << " -> ";
+            for (int j : adj[i]) {
+                cout << j << " ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+int main() {
+    Graph g(5);
+    g.addEdge(0, 1);
+    g.addEdge(0, 4);
+    g.addEdge(1, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 3);
+    g.addEdge(3, 4);
+
+    cout << "Graph adjacency list:" << endl;
+    g.display();
+
+    return 0;
+}
