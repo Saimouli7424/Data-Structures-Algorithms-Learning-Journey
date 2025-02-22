79ac567c4c8e2146e818c30065c620fcd275d2ad
Sai Mouli Bandi <saimoulispeed@gmail.com>
2025-02-22 21:52:10 +0530

    repractice of hash

---
+++ Day12/hashrepractice.cpp
@@ -0,0 +1,31 @@
+//prints the occurence of a number
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the size of an array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of an array: ";
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int hash[13] = {0};
+    for(int i=0;i<n;i++)
+    {
+        hash[arr[i]]++;
+    }
+
+    int q;
+    cout<<"Enter the number of queries:";
+    cin>>q;
+    while(q--)
+    {
+        int x;
+        cout<<"Enter the number to check occurrence: ";
+        cin>>x;
+        cout<<"For the number: "<<x<<" Occurred is: "<<hash[x]<<endl;
+    }
+    return 0;
+}
